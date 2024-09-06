85e174f343bf16ccac1ef6cfcb0431bdba54e59b
Chudakoff <chudakoff@yahoo.com>
2024-09-06 12:40:16 +0300

    pause of project

--- config/main/views.py
+++ config/main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', '123', 'Bye Bye'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'football'
+        }
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
